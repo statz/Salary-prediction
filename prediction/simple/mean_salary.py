88df8ebe9a98dcb5e8d733695f0d31da42eadba2
Artem Kotin <statz@users.noreply.github.com>
2017-07-29 09:38:29 +0300

    Mean salary by words

---
+++ prediction/simple/mean_salary.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import os
+import time
+
+time.clock()
+ud = ["down", "up"]
+data_path = os.path.split(os.path.split(os.getcwd())[0])[0] + "//test_train//"
+for b in ud:
+    train_dict = {}
+    train_x = pd.read_csv(data_path+b+"_x_train_normalized.csv")["TextOfVacancy"]
+    train_y = pd.read_csv(data_path+b+"_y_train.csv")[b]
+    for i in range(train_x.shape[0]):
+        words = train_x.ix[i].split(" ")
+        for w in words:
+            val = train_dict.get(w)
+            if val is None:
+                train_dict.update({w: [1, train_y.ix[i]]})
+            else:
+                train_dict.update({w: [val[0]+1, val[1]+train_y.ix[i]]})
+    mean_sal = []
+    for i in train_dict:
+        val = train_dict.get(i)
+        mean_sal.append([i, val[1]/val[0]])
+    df = pd.DataFrame(mean_sal, columns=["word", "mean_sal"])
+    df.to_csv(b+"_words_sal.csv", encoding='utf-8', index=False)
+    print("done")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
