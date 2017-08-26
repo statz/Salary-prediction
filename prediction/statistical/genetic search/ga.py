05440ac3bc2c5161a4e3084dcca97f20de165b16
Artem Kotin <statz@users.noreply.github.com>
2017-08-26 10:51:20 +0300

    ga

--- prediction/statistical/genetic search/ga.py
+++ prediction/statistical/genetic search/ga.py
@@ -14,6 +14,7 @@ class GA:
     __test_y = None
 
     __current_population = None
+    __fitness_current = []
 
     def __init__(self, n_epoch, population_size, n_gens, p_mutation, estimator):
         self.__n_epoch = n_epoch
@@ -34,30 +35,41 @@ class GA:
     def __population_creating(self):
         self.__current_population = np.random.choice([0, 1],
                                                      size=(self.__population_size, self.__n_gens), p=[0.5, 0.5])
+        for i in range(self.__population_size):
+            self.__fitness_current.append(self.__evaluate(self.__current_population[i, :]))
 
     def __search(self):
+        new_gen = np.empty([self.__population_size, self.__n_gens])
         for i in range(self.__n_epoch):
-            self.__make_new_generation()
+            new_gen[i, :] = self.__make_new_generation()[:]
+            fitness_new = []
+            for j in range(self.__population_size):
+                fitness_new.append(self.__evaluate(new_gen[j, :]))
+            joint_population = np.concatenate((self.__current_population, new_gen))
+            fitness_new = self.__fitness_current+fitness_new
+            next_ind = np.argpartition(fitness_new, -self.__population_size)[-self.__population_size:]
+            self.__current_population = joint_population[next_ind, :]
+            self.__fitness_current = fitness_new[next_ind]
 
     def __make_new_generation(self):
-        fitness_current = []
-        for i in range(self.__population_size):
-            fitness_current.append(self.__evaluate(self.__current_population[i, :]))
         new_population = []
         for i in range(self.__population_size):
-            a, b = self.__roulette_wheel_selection(fitness_current)
+            a, b = self.__roulette_wheel_selection()
             new = (self.__double_point_crossingover(self.__current_population[a, :], self.__current_population[b, :]))
             new_population.append(self.__mutation(new))
+        return new_population
 
-    def __roulette_wheel_selection(self, fitness_current):
-        cum = np.cumsum(fitness_current)/np.sum(fitness_current)
+    def __roulette_wheel_selection(self):
+        cum = np.cumsum(self.__fitness_current)/np.sum(self.__fitness_current)
         a = np.random.ranf(1)
         i = 0
-        while a < cum[i]: i += 1
+        while a < cum[i]:
+            i += 1
         j = 0
         while i == j:
             b = np.random.ranf(1)
-            while b < cum[j]: j += 1
+            while b < cum[j]:
+                j += 1
         return [i-1, j-1]
 
     def __double_point_crossingover(self, first, second):
@@ -73,5 +85,3 @@ class GA:
         self.__estimator.fit(self.__train_x[:, np.nonzero(genome)], self.__train_y)
         return self.__estimator.score(self.__test_x[:, np.nonzero(genome)], self.__test_y)
 
-g = GA(2, 5, 5, 5)
-g.optimize()
--- prediction/statistical/genetic search/genetic_fe.py
+++ prediction/statistical/genetic search/genetic_fe.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import os
 import numpy as np
+import sys
+sys.path.append(os.getcwd())
+from ga import GA
 
 ud = ["down", "up"]
 data_path = os.path.split(os.path.split(os.getcwd())[0])[0] + "//test_train//"
+
+ga = GA(1,2,3,4,5)
