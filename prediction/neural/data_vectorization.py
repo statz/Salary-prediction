d6f668c68418cd535770d248dc5f75806c49febc
Artem Kotin <art.kotin@yandex.ru>
2017-10-23 21:45:50 +0300

    update

--- prediction/neural/data_vectorization.py
+++ prediction/neural/data_vectorization.py
@@ -5,44 +5,50 @@ import numpy as np
 import h5py
 
 data_path = os.getcwd().split("\\prediction")[0]+"\\test_train\\"
-ud = ["up", "down"]
+ud = ["down"]
 for l in ud:
     train_data_x = pd.read_csv(data_path + l+"_x_train.csv")["TextOfVacancy"]
     test_data_x = pd.read_csv(data_path + l+"_x_test.csv")["TextOfVacancy"]
     #train_y = pd.read_csv(data_path + l + "_y_train.csv")[l].as_matrix()
     #test_y = pd.read_csv(data_path + l + "_y_test.csv")[l].as_matrix()
-
-    median_len = int(np.median(list(map(len, train_data_x))))
-    vect_train_x = np.zeros([len(train_data_x), 50*200])
+    cl = []
+    for i in train_data_x:
+        cl.append(len(str(i).split(" ")))
+    median_len = int(np.median(cl))
+    ind_train_x = np.zeros([len(train_data_x), median_len])
     model = w2v.load(os.getcwd()+"\\data\\"+l+"_model")
+    vocabulary = list(model.wv.vocab)
+    d = {vocabulary[w]: w for w in range(len(vocabulary))}
+    vocabulary = set(vocabulary)
     for i in range(train_data_x.shape[0]):
+        print(i)
         words = str(train_data_x.iloc[i]).split(' ')
         j = 0
         for k in range(len(words)):
-            if words[k] in model.wv.vocab:
-                 vect_train_x[i][50*j:50*(j+1)] = model.wv[words[k]]
+            if words[k] in vocabulary:
+                 ind_train_x[i][j] = d[words[k]]
                  j += 1
-                 if j == 200:
+                 if j == median_len:
                      break
-    print("2123")
-    h5f = h5py.File(os.getcwd()+"\\data\\"+l+"_vect_train_x.h5", 'w')
-    h5f.create_dataset('dataset_1', data=vect_train_x)
-    h5f.close()
-    print("0")
-    del vect_train_x
+    pd.DataFrame(ind_train_x).to_csv(os.getcwd()+"\\data\\"+l+"_ind_train_x.csv", index=False)
     print("1")
-    vect_test_x = np.zeros([len(test_data_x), 100*median_len])
+    ind_test_x = np.zeros([len(test_data_x), median_len])
     for i in range(test_data_x.shape[0]):
+        print(i)
         words = str(test_data_x.iloc[i]).split(' ')
         j = 0
         for k in range(len(words)):
-            if words[k] in model.wv.vocab:
-                vect_test_x[i][50 * j:50 * (j + 1)] = model.wv[words[k]]
+            if words[k] in vocabulary:
+                ind_test_x[i][j] = d[words[k]]
                 j += 1
-                if j == 200:
+                if j == median_len:
                     break
-    h5f = h5py.File(os.getcwd()+"\\data\\"+l+"_vect_test_x.h5", 'w')
-    h5f.create_dataset('dataset_1', data=vect_test_x)
-    h5f.close()
-    del vect_test_x
+    pd.DataFrame(ind_test_x).to_csv(os.getcwd()+"\\data\\" + l + "_ind_test_x.csv", index=False)
     print("2")
+    vectors_repr = np.empty([len(vocabulary), 50])
+    i = 0
+    for w in vocabulary:
+        vectors_repr[i, :] = model.wv[w]
+        i += 1
+    pd.DataFrame(vectors_repr).to_csv(os.getcwd()+"\\data\\" + l + "_vectors_repr.csv", index=False)
+
